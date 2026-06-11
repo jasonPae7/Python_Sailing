31af7b6e845daf3f318b2922a3f3305ee0d7706c
jasonPae7 <JasonFuzy@hotmail.com>
2026-06-11 16:55:23 +0800

    完成Python crash course third 5 练习完成

--- P_5_260611.py
+++ P_5_260611.py
@@ -16,13 +16,24 @@ from zoneinfo import available_timezones
 #         print(f"Sorry We don't have {requested_topping}.")
 # print("\nFinished making your pizza!")
 
-user_list = ['Joy','Mike','Admin','Peter']
-del user_list[:len(user_list)]
-if user_list:
-    for user in user_list:
-        if user == 'Admin':
-            print("Would you like to see a status report?")
-        else:
-            print(f"Hello,{user} happy to login.")
-else:
-    print("We need to find some users.")
+# user_list = ['Joy','Mike','Admin','Peter']
+# del user_list[:len(user_list)]
+# if user_list:
+#     for user in user_list:
+#         if user == 'Admin':
+#             print("Would you like to see a status report?")
+#         else:
+#             print(f"Hello,{user} happy to login.")
+# else:
+#     print("We need to find some users.")
+
+number_sequen = [1,2,3,4,5,6,7,8,9]
+for num in number_sequen:
+    if num == 1:
+        print(f"{num}st")
+    elif num == 2:
+        print(f"{num}nd")
+    elif num == 3:
+        print(f"{num}rd")
+    else:
+        print(f"{num}th")
