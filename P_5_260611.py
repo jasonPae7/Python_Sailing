abb95289f8719fe92f39115fa02fe523270e5d34
jasonPae7 <JasonFuzy@hotmail.com>
2026-06-11 16:13:06 +0800

    完成Python crash course third 5.4.2 code

---
+++ P_5_260611.py
@@ -0,0 +1,7 @@
+requested_toppings = []
+if requested_toppings:
+    for topping in requested_toppings:
+        print(f"Adding {topping}")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want plain pizza?")
