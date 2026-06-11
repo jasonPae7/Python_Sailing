4d9f8385ad53b72bbf2f90430a6b4a5f0ddfaf7e
jasonPae7 <JasonFuzy@hotmail.com>
2026-06-11 16:50:12 +0800

    完成Python crash course third 5 练习

--- P_5_260611.py
+++ P_5_260611.py
@@ -7,11 +7,22 @@
 #     print("Are you sure you want plain pizza?")
 from zoneinfo import available_timezones
 
-available_toppings = ['mushrooms','olives','green_peppers','pepperoin','pineapple','extra cheese']
-requested_toppings = ['mushrooms','french fries','extra cheese']
-for requested_topping in requested_toppings:
-    if requested_topping in available_toppings:
-        print(f"Adding {requested_topping}.")
-    else:
-        print(f"Sorry We don't have {requested_topping}.")
-print("\nFinished making your pizza!")
+# available_toppings = ['mushrooms','olives','green_peppers','pepperoin','pineapple','extra cheese']
+# requested_toppings = ['mushrooms','french fries','extra cheese']
+# for requested_topping in requested_toppings:
+#     if requested_topping in available_toppings:
+#         print(f"Adding {requested_topping}.")
+#     else:
+#         print(f"Sorry We don't have {requested_topping}.")
+# print("\nFinished making your pizza!")
+
+user_list = ['Joy','Mike','Admin','Peter']
+del user_list[:len(user_list)]
+if user_list:
+    for user in user_list:
+        if user == 'Admin':
+            print("Would you like to see a status report?")
+        else:
+            print(f"Hello,{user} happy to login.")
+else:
+    print("We need to find some users.")
