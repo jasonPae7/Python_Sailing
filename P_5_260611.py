7bb2950cb4e0cb3863848d64154ca71975b58ec9
jasonPae7 <JasonFuzy@hotmail.com>
2026-06-11 16:31:25 +0800

    完成Python crash course third 5.4.3 code

--- P_5_260611.py
+++ P_5_260611.py
@@ -1,7 +1,17 @@
-requested_toppings = []
-if requested_toppings:
-    for topping in requested_toppings:
-        print(f"Adding {topping}")
-    print("\nFinished making your pizza!")
-else:
-    print("Are you sure you want plain pizza?")
+# requested_toppings = []
+# if requested_toppings:
+#     for topping in requested_toppings:
+#         print(f"Adding {topping}")
+#     print("\nFinished making your pizza!")
+# else:
+#     print("Are you sure you want plain pizza?")
+from zoneinfo import available_timezones
+
+available_toppings = ['mushrooms','olives','green_peppers','pepperoin','pineapple','extra cheese']
+requested_toppings = ['mushrooms','french fries','extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry We don't have {requested_topping}.")
+print("\nFinished making your pizza!")
