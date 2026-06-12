2e93ef25c3f00e68c88baee45b91df983b494e4a
jasonPae7 <JasonFuzy@hotmail.com>
2026-06-12 10:49:25 +0800

    完成Python crash course third 6 练习完成

---
+++ P_6_260612.py
@@ -0,0 +1,27 @@
+# Pets = []
+# #PetDic = {'dog':'Jack','cat':'milk','tiger':'byson'}
+# Pets.append({'dog':'Jack'})
+# Pets.append({'cat':'milk'})
+# Pets.append({'tiger':'byson'})
+#
+# for pet in Pets:
+#     print(pet)
+
+# site_places = 'GD'
+# favorite_places = {
+#     'jack':['XZ','FJ'],
+#     'miles':['BJ','SH'],
+#     'peter':['SC','GD'],
+# }
+# for name,place in favorite_places.items():
+#     if site_places in place:
+#         print(f"{name} to {place}")
+
+# cities = {
+#     'Beijing':{'country':'Beijing','population':30000,'fact':'nightmare'},
+#     'Toyota':{'country':'Japan','population':1000,'fact':'evil'},
+#     'Newyork':{'country':'USA','population':2000,'fact':'beauty'},
+# }
+#
+# for city,info in cities.items():
+#     print(f"\nCity: {city}\n\t info {info}")
